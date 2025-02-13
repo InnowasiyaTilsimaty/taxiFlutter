c85f2e878c6206850c62d8f80474122bd2ab22d4
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2025-02-13 13:01:54 +0500

    splash view

---
+++ lib/view/splash/splash_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../configs/assets.dart';
+import '../../configs/components/indicators.dart';
+import '../../configs/routes/routes.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(
+        context,
+        RouteNames.home,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(height: 10),
+            SvgPicture.asset(Assets.logo),
+            const LoadingDotsIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/view.dart
@@ -0,0 +1,2 @@
+export 'package:taksist/view/home/home_view.dart';
+export 'package:taksist/view/splash/splash_view.dart';
