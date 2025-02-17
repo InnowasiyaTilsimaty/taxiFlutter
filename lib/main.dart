516662fbd36033674e3cd074b8dddad1ae4bcbfc
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2025-02-17 11:28:47 +0500

    get it

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 import 'configs/routes/routes.dart';
 import 'configs/theme/app_theme.dart';
+import 'view_model/view_model.dart';
+
+GetIt getIt = GetIt.instance;
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  getIt.registerLazySingleton<OrderViewModel>(OrderViewModel.new);
+
   runApp(const TaksistApp());
 }
 
@@ -11,13 +18,18 @@ class TaksistApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Taksist',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => getIt<OrderViewModel>()),
+      ],
+      child: MaterialApp(
+        title: 'Taksist',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.light,
         themeMode: ThemeMode.light,
         initialRoute: RouteNames.splash,
         routes: Navigation.routes,
+      ),
     );
   }
 }
